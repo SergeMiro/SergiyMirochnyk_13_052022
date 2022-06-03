24118935b587e90217a3c8ec9fdff7b3ea29a343
MIROCHNYK <76389860+SergeMiro@users.noreply.github.com>
2022-06-03 16:21:42 +0200

    Pages : home + signin + user

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import '../utils/Style/main.css'
+import Logo from '../assets/Images/argentBankLogo.png'
+import { Link } from 'react-router-dom';
+
+
+function Header() {
+
+  const userSigned = true //envoyé par le store
+
+  return (
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
+        <img
+          className="main-nav-logo-image"
+          src={Logo}
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      {(userSigned) ? (
+        <div>
+        <Link className="main-nav-item" to="/sign-in">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+        </Link>
+      </div>
+      ) : (
+        <div>
+        <Link className="main-nav-item" to="/user">
+          <i className="fa fa-user-circle"></i>
+          Tony
+        </Link>
+        <Link classNameName="main-nav-item" to="/">
+          <i classNameName="fa fa-sign-out"></i>
+          Sign Out
+        </Link>
+      </div>
+      )
+      }
+    </nav>
+  );
+}
+
+export default Header;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './utils/Style/main.css';
+import App from './pages/home';
+import SignIn from './pages/signin';
+import User from './pages/user'
+import Header from './components/header';
+import Footer from './components/footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route path="/sign-in">
+          <SignIn />
+        </Route>
+        <Route path="/user">
+          <User />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
